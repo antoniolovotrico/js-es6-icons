908b39124e47659ef1856d4cb72d834f945d5e28
Antonio <antonio.lovotrico@icloud.com>
2020-12-10 20:51:25 +0100

    start working on milestone number 3

--- milestone3/icons.js
+++ milestone3/icons.js
@@ -1,13 +1,136 @@
-$(document).ready(function () {
+$(function () {
  // icone come da milestone 1
 
+ const icone = [
+  {
+    name : "gatto",
+    prefix : "fas",
+    type : "fa-cat",
+    family : "animali"
+  },
+  {
+    name : "pesce",
+    prefix : "fas",
+    type : "fa-fish",
+    family : "animali"
+  },
+  {
+    name : "cane",
+    prefix : "fas",
+    type : "fa-dog",
+    family : "animali"
+  },
+  {
+    name : "cavallo",
+    prefix : "fas",
+    type : "fa-horse",
+    family : "animali"
+  },
+  {
+    name : "drago",
+    prefix : "fas",
+    type : "fa-dragon",
+    family : "animali"
+  },
+  {
+    name : "ippopotamo",
+    prefix : "fas",
+    type : "fa-hippo",
+    family : "animali"
+  },
+  {
+    name : "colomba",
+    prefix : "fas",
+    type : "fa-dove",
+    family : "animali"
+  },
+  {
+    name : "corvo",
+    prefix : "fas",
+    type : "fa-crow",
+    family : "animali"
+  },
+  {
+    name : "carota",
+    prefix : "fas",
+    type : "fa-carrot",
+    family : "vegetali"
+  },
+  {
+    name : "limone",
+    prefix : "fas",
+    type : "fa-lemon",
+    family : "vegetali"
+  },
+  {
+    name : "mela",
+    prefix : "fas",
+    type : "fa-apple-alt",
+    family : "vegetali"
+  },
+  {
+    name : "Peperoncino",
+    prefix : "fas",
+    type : "fa-pepper-hot",
+    family : "vegetali"
+  },
+  {
+    name : "Ninja",
+    prefix : "fas",
+    type : "fa-user-ninja",
+    family : "utenti"
+  },
+  {
+    name : "Agente",
+    prefix : "fas",
+    type : "fa-user-secret",
+    family : "utenti"
+  },
+  {
+    name : "Laureato",
+    prefix : "fas",
+    type : "fa-user-graduate",
+    family : "utenti"
+  },
+  {
+    name : "Astronauta",
+    prefix : "fas",
+    type : "fa-user-astronaut",
+    family : "utenti"
+  }
+];
 
   // colori come da milestone 2
  
+  const containerIcon = document.getElementById("icons");
 
   //aggiungiamo dei colori come da milestone 2
-
   //inseriamo le icone nel container come da milestone 2
+  const colors = icone.map((index) => {
+    console.log(index);
+    if (index.family === "animali") {
+      return containerIcon.innerHTML += `
+      <div>
+        <i class="${index.prefix} ${index.type}" style="color:blue"></i>
+        <div class="title">${index.name}</div>
+    </div>
+    `; 
+    }else if (index.family === "vegetali") {
+      return containerIcon.innerHTML += `
+      <div>
+        <i class="${index.prefix} ${index.type}" style="color:orange"></i>
+        <div class="title">${index.name}</div>
+    </div>
+    `;
+    }else {
+      return containerIcon.innerHTML += `
+      <div>
+        <i class="${index.prefix} ${index.type}" style="color:purple"></i>
+        <div class="title">${index.name}</div>
+    </div>
+    `;
+    }
+  });
 
   //estrapoliamo i tipi di icone
 
